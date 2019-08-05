6f6bf3bad1755d36ff53b706c4ec6e4283b26a6b
hanlinz <761323865@qq.com>
2019-08-05 22:43:22 +0800

    添加demo2
    通过vue-cli构建项目

---
+++ demo2/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import elementUI from 'element-ui'
+
+Vue.use(elementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
